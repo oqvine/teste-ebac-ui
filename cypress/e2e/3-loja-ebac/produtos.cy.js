58af5d88c3aff2edf3c3a2a88a5623ecdfc3dc55
Vinicius Blasque <oqvine@live.com>
2024-03-05 17:27:41 -0300

    Massa de dados em lista

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -14,17 +14,32 @@ describe('Funcionalidade: Produtos', () => {
             cy.get('#tab-title-description > a').should('contain', 'Descrição')
     });
 
-    it.only('Deve buscar um produto com sucesso', () => {
+    it('Deve buscar um produto com sucesso', () => {
         let produto = 'Ajax Full-Zip Sweatshirt'
         produtosPage.buscarProduto(produto)
         cy.get('.product_title').should('contain', produto)
     });
 
     it('Deve visitar a página de um produto', () => {
-        
+        produtosPage.visitarProduto('Ajax Full-Zip Sweatshirt')
     });
 
     it('Deve adicionar um produto no carrinho', () => {
-        
+        let qtd = 5
+        produtosPage.buscarProduto('Aero Daily Fitness Tee')
+        produtosPage.addProdutoCarrinho('XS', 'Black', qtd)
+
+        cy.get('.woocommerce-message').should('contain', qtd +' × “Aero Daily Fitness Tee” foram adicionados no seu carrinho.')
+    });
+
+    it.only('Deve adicionar um produto no carrinho buscando na massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+
+            produtosPage.buscarProduto(dados[1].nomeProduto)
+            produtosPage.addProdutoCarrinho(dados[1].tamanho, dados[1].cor, dados[1].quantidade)
+
+            cy.get('.woocommerce-message').should('contain', dados[1].nomeProduto)
+
+        })
     });
 });
--- cypress/support/page-objects/produtos.page.js
+++ cypress/support/page-objects/produtos.page.js
@@ -15,7 +15,19 @@ class ProdutosPage {
             .click()
     }
 
-    addProdutoCarrinho() {
+    visitarProduto(nomeProduto) {
+        // cy.visit(`produtos/${nomeProduto}`)
+        const urlFormatada = nomeProduto.replace(/ /g, '-')
+        cy.visit(`produtos/${urlFormatada}`)
+
+    }
+
+    addProdutoCarrinho(tamanho, cor, quantidade) {
+        cy.get(`.button-variable-item-${tamanho}`).click()
+        cy.get(`.button-variable-item-${cor}`).click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+
 
     }
 
